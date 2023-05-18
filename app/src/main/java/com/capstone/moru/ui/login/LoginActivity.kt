11a5d2ab562295f92fa6e1776f1562cda3c7143a
Krisna <krisnaerlangga08@gmail.com>
2023-05-18 13:03:29 +0700

    add: font style and theme :rocket:

--- app/src/main/java/com/capstone/moru/ui/login/LoginActivity.kt
+++ app/src/main/java/com/capstone/moru/ui/login/LoginActivity.kt
@@ -3,10 +3,16 @@ package com.capstone.moru.ui.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.capstone.moru.R
+import com.capstone.moru.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private var _binding: ActivityLoginBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        _binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
---
+++ app/src/main/java/com/capstone/moru/ui/register/RegisterActivity.kt
@@ -0,0 +1,13 @@
+
+package com.capstone.moru.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.moru.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
