7d8b3578ae9c623f519d03084adfae180a86fbb6
Krisna <krisnaerlangga08@gmail.com>
2023-05-18 16:18:42 +0700

    add: register activity, connect register login :rocket:

--- app/src/main/java/com/capstone/moru/ui/login/LoginActivity.kt
+++ app/src/main/java/com/capstone/moru/ui/login/LoginActivity.kt
@@ -1,11 +1,13 @@
 package com.capstone.moru.ui.login
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowManager
 import com.capstone.moru.databinding.ActivityLoginBinding
+import com.capstone.moru.ui.register.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
     private var _binding: ActivityLoginBinding? = null
@@ -18,6 +20,10 @@ class LoginActivity : AppCompatActivity() {
 
         setupView()
 
+        binding.tvRegister.setOnClickListener {
+            val intentToRegister = Intent(this, RegisterActivity::class.java)
+            startActivity(intentToRegister)
+        }
     }
 
     private fun setupView() {
--- app/src/main/java/com/capstone/moru/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/capstone/moru/ui/register/RegisterActivity.kt
@@ -1,13 +1,33 @@
 
 package com.capstone.moru.ui.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.capstone.moru.R
+import com.capstone.moru.databinding.ActivityRegisterBinding
+import com.capstone.moru.ui.login.LoginActivity
 
 class RegisterActivity : AppCompatActivity() {
+
+    private var _binding: ActivityRegisterBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+
+        _binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+
+        binding.tvLogin.setOnClickListener {
+            val intentToLogin = Intent(this, LoginActivity::class.java)
+            startActivity(intentToLogin)
+        }
+    }
+
+    private fun setupView() {
+        supportActionBar?.hide()
     }
 }
