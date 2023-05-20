0728beb023199b88f8dcf62de34613438b05cae1
diladelia <diladelia0202@gmail.com>
2023-05-20 22:49:11 +0700

    add: intro activity

---
+++ app/src/main/java/com/capstone/moru/ui/fill/FillProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.capstone.moru.ui.fill
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FillProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fill_profile)
+    }
+}
