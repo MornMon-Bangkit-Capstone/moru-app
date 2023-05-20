241d86c48ba3d07cc458720eeb5d5430ce10bb8d
Krisna <krisnaerlangga08@gmail.com>
2023-05-20 20:34:53 +0700

    add: fragments for all pages :rocket:

---
+++ app/src/main/java/com/capstone/moru/ui/alarm/AlarmActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.moru.ui.alarm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.moru.R
+
+class AlarmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm)
+    }
+}
