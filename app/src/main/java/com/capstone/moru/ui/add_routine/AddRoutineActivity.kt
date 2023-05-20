8544a6d24218563b690a2e967dcbe66bcd028dab
Krisna <krisnaerlangga08@gmail.com>
2023-05-20 23:51:25 +0700

    add: item pick routine :rocket:

---
+++ app/src/main/java/com/capstone/moru/ui/add_routine/AddRoutineActivity.kt
@@ -0,0 +1,11 @@
+package com.capstone.moru.ui.add_routine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddRoutineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_routine)
+    }
+}
