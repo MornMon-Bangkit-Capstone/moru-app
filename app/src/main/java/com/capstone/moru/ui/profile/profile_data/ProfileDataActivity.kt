a7da1fe9218f721db4d8f8e1403916a6cc2bbf3e
Krisna <krisnaerlangga08@gmail.com>
2023-05-21 14:15:43 +0700

    refactor: change fragments to activity :hammer:

---
+++ app/src/main/java/com/capstone/moru/ui/profile/profile_data/ProfileDataActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.moru.ui.profile.profile_data
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.moru.R
+
+class ProfileDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_data)
+    }
+}
