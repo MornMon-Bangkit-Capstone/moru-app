f57b1b103837d75d2ca054321cd1a4b5d508462d
Krisna <krisnaerlangga08@gmail.com>
2023-05-28 15:29:04 +0700

    add: custom fab button :rocket:

--- app/src/main/java/com/capstone/moru/ui/MainActivity.kt
+++ app/src/main/java/com/capstone/moru/ui/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.capstone.moru.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -15,6 +18,21 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
+
+    private val rotateOpen: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.rotate_open_anim)
+    }
+    private val rotateClose: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.rotate_close_anim)
+    }
+    private val fromBottom: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim)
+    }
+    private val toBottom: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim)
+    }
+    private var clicked: Boolean = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,9 +57,62 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navViewController, appBarConfiguration)
         navView.setupWithNavController(navViewController)
 
-        binding.fab.setOnClickListener {
+        binding.fabMenu.setOnClickListener {
+            onFabMenuClicked()
+        }
+
+        binding.fabPickRoutine.setOnClickListener {
             val intentToPickRoutine = Intent(this, PickRoutineActivity::class.java)
             startActivity(intentToPickRoutine)
+        }
+    }
+
+    private fun onFabMenuClicked() {
+        setVisibility(clicked)
+        setAnimation(clicked)
+        setClickable(clicked)
+        clicked = !clicked
+
+    }
+
+
+    private fun setVisibility(clicked: Boolean) {
+        if (!clicked) {
+            binding.fabPickRoutine.visibility = View.VISIBLE
+            binding.fabAddRoutine.visibility = View.VISIBLE
+            binding.tvAddRoutine.visibility = View.VISIBLE
+            binding.tvPickRoutine.visibility = View.VISIBLE
+        } else {
+            binding.fabPickRoutine.visibility = View.INVISIBLE
+            binding.fabAddRoutine.visibility = View.INVISIBLE
+            binding.tvAddRoutine.visibility = View.INVISIBLE
+            binding.tvPickRoutine.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun setAnimation(clicked: Boolean) {
+        if (!clicked) {
+            binding.fabAddRoutine.startAnimation(fromBottom)
+            binding.tvAddRoutine.startAnimation(fromBottom)
+            binding.fabPickRoutine.startAnimation(fromBottom)
+            binding.tvPickRoutine.startAnimation(fromBottom)
+            binding.fabMenu.startAnimation(rotateOpen)
+        } else {
+            binding.fabAddRoutine.startAnimation(toBottom)
+            binding.tvAddRoutine.startAnimation(toBottom)
+            binding.fabPickRoutine.startAnimation(toBottom)
+            binding.tvPickRoutine.startAnimation(toBottom)
+            binding.fabMenu.startAnimation(rotateClose)
+        }
+    }
+
+    private fun setClickable(clicked: Boolean) {
+        if (!clicked) {
+            binding.fabPickRoutine.isClickable = true
+            binding.fabAddRoutine.isClickable = true
+        } else {
+            binding.fabPickRoutine.isClickable = false
+            binding.fabAddRoutine.isClickable = false
         }
     }
 
