5bce0559293f36261d673fb1e0fbfe19118bdf90
diladelia <diladelia0202@gmail.com>
2023-06-09 20:28:13 +0700

    modified

---
+++ app/src/main/java/com/capstone/moru/ui/fill/FillBookPrefActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.moru.ui.fill
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.moru.R
+
+class FillBookPrefActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fill_book_pref)
+    }
+}
